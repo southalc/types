54f9a1fb76ff71fa71d0b9f98710b6e03ff77fda
Chris Southall <southallc@gmail.com>
2021-02-26 22:23:28 -0600

    basic rspec tests

--- spec/defines/binary_spec.rb
+++ spec/defines/binary_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 describe 'types::binary' do
-  let(:title) { 'namevar' }
-  let(:params) do
-    {}
-  end
+  let(:title) { '/tmp/binary_spec.tmp' }
+  let(:params) {{
+    :properties => {
+      :ensure  => 'file',
+      :content  => 'VHlwZXMgdW5pdCB0ZXN0Cg=='
+    }
+  }}
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
+      if os.match(%r{windows}i)
+        let(:title) { 'C:\\Temp\\binary_spec.tmp' }
+      else
+        let(:title) { '/tmp/binary_spec.tmp' }
+      end
       it { is_expected.to compile }
     end
   end
--- spec/defines/type_spec.rb
+++ spec/defines/type_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe 'types::type' do
-  let(:title) { 'namevar' }
-  let(:params) do
-    {}
-  end
-
+  let(:title) { 'notify' }
+  let(:params) {{
+    :hash => {
+      :unit_test => {
+        :message  => 'Unit test'
+      }
+    }
+  }}
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
